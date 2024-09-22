193f24b6687e95b12fd0ea46f071f6b57ef14c0b
Hassaan Abid <145790867+kinghassaan7@users.noreply.github.com>
2024-09-22 12:42:06 +0500

    Add files via upload

---
+++ route.mjs
@@ -0,0 +1,16 @@
+import express from "express"
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send("This is home page")
+})
+
+app.get('/about', (req, res) => {
+    res.send("This is about page")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is listening on http://localhost:${PORT}`);
+})
