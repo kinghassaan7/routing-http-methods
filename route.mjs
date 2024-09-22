8149539f3a9bd1b466652a6c4bafe9176e64113a
Hassaan Abid <145790867+kinghassaan7@users.noreply.github.com>
2024-09-22 12:43:56 +0500

    Update route.mjs

--- route.mjs
+++ route.mjs
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("This is home page")
+    res.send("This is home page");
 })
 
 app.get('/about', (req, res) => {
